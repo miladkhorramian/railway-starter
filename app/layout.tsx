f71aae9f0f4b48b02b2de70e55b9840c8f2f230d
Milad Khorramian <mahdiar.khorramian@yahoo.com>
2025-02-22 20:27:32 +0330

    ADD: Added login route.

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="fa" dir="rtl">
       <body className={`${IRANSansX.className}`}>
         <SidebarProvider>
           <>
